3614138e39c8dcc52031a39877016127f38f51f5
dtipei <tipei_denis@yahoo.com>
2022-06-27 14:10:00 -0700

    updating script

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,4 +19,38 @@ window.onload = function() {
         container.Clicked(event, currentTimeblocks);
         });
     setTimeblockText(currentTimeblocks);
+};
+
+function getCurrentTimeblocks() {
+    var currentTimeblocks = localStorage.getItem('timeblockObjects');
+    return currentTimeblocks ? JSON.parse(currentTimeblocks) : [];
 }
+
+// using the functions to display the current date
+
+function displayCurrentDate(currentTime) {
+    document.getElementById('currentDay')
+        .textContent = currentTime.format('dddd, MMM Do');
+}
+
+// display all rows of timeblocks
+
+function displayTimeblockRows(currentTime) {
+    var currentHour = currentTime.hour();
+    for (let i = 9; i <= 5; i++) {
+        var timeblock = createTimeblockRow(i);
+        var hourCol = createCol(createHourDiv(i), 1);
+        var textArea = createCol(createTextArea(i, currentHour), 10);
+        var saveBtn = createCol(createSaveBtn(i), 1);
+        appendTimeblockColumns(timeblock, hourCol, textArea, saveBtn);
+        document.querySelector('.container').appendChild(timeblock);
+    }
+}
+
+function createTimeblockRow(hourId) {
+    var timeblock = document.createElement('div');
+    timeblock.classList.add('row');
+    timeblock.id = timeblock-$(hourId);
+    return timeblock;
+}
+
